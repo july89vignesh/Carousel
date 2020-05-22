76a44d76f46335bdeef877a060ed13c803ce3cf1
july89vignesh <63089305+july89vignesh@users.noreply.github.com>
2020-05-22 12:07:32 +0530

    Update ImgComp.js

--- src/ImgComp.js
+++ src/ImgComp.js
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function ImgComp({ src }) {
-  let imgStyles = {
-    width: 100 + "%",
-    height: "auto"
-  };
-  return <img src={src} alt="slide-img" style={imgStyles} />;
+  return <img src={src} alt="slide-img" />;
 }
